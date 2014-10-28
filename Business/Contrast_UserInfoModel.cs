5b40de20c29c6976a7474151f7be20295884a98e
aicike <176534021@qq.com>
2014-10-28 16:22:11 +0800

    add

---
+++ Business/Contrast_UserInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entity;
+
+namespace Business
+{
+    public class Contrast_UserInfoModel : BaseModel<Contrast_UserInfo>
+    {
+        public List<Contrast_UserInfo> UserContrast()
+        {
+            return null;
+        }
+    }
+}
