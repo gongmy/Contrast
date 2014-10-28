e3b9caf19dc4be86ecdb7ea6559e5f9c4e8f61fc
gongmy <gmyao.china@gmail.com>
2014-10-28 16:23:40 +0800

    adds

---
+++ Business/OrganizationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entity;
+
+namespace Business
+{
+    public class OrganizationModel : BaseModel<Contrast_Organization>
+    {
+    }
+}
---
+++ Contrast/Controllers/OrganizationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business;
+
+namespace CustomTab1.Controllers
+{
+    public class OrganizationController : Controller
+    {
+        //
+        // GET: /Organization/
+
+        /// <summary>
+        /// 组织机构列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewBag.Menu = 3;
+            ViewBag.Title = "组织机构列表";
+
+            OrganizationModel omodel = new OrganizationModel();
+            var organization = omodel.List().ToList();
+
+            return View(organization);
+        }
+
+        public ActionResult OrgContrast()
+        {
+            ViewBag.Menu = 3;
+            ViewBag.Title = "与组织匹配的用户信息";
+            return View();
+        }
+
+    }
+}
